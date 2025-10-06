c15002e32a4061ebf4a3622587ad60112b4157c2
Cameron Smith <cameron.ray.smith@gmail.com>
2025-10-06 00:48:48 -0400

    feat: add containers flake-parts module with fd example
    Create containers module for multi-arch Docker images using flocken.
    Example container with fd tool that can be run like a nix shell.

---
+++ modules/flake-parts/containers.nix
@@ -0,0 +1,63 @@
+{ inputs, ... }:
+{
+  perSystem =
+    {
+      pkgs,
+      lib,
+      system,
+      ...
+    }:
+    let
+      isLinux = pkgs.stdenv.isLinux;
+
+      # Build a minimal container image with a package
+      # Makes it work like: docker run <name>:latest --version
+      mkToolContainer =
+        {
+          name,
+          package,
+          tag ? "latest",
+        }:
+        pkgs.dockerTools.buildLayeredImage {
+          inherit name tag;
+          contents = [
+            pkgs.bashInteractive
+            pkgs.coreutils
+            package
+          ];
+          config = {
+            Cmd = [ "${package}/bin/${name}" ];
+            Env = [
+              "PATH=${package}/bin:${pkgs.coreutils}/bin:${pkgs.bashInteractive}/bin"
+            ];
+            Labels = {
+              "org.opencontainers.image.description" = "Minimal container with ${name}";
+              "org.opencontainers.image.source" = "https://github.com/cameronraysmith/nix-config";
+            };
+          };
+        };
+
+      # Systems to build images for (both architectures)
+      imageSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    in
+    {
+      packages = lib.optionalAttrs isLinux {
+        fdContainer = mkToolContainer {
+          name = "fd";
+          package = pkgs.fd;
+        };
+      };
+
+      legacyPackages = {
+        fdManifest = inputs.flocken.legacyPackages.${system}.mkDockerManifest {
+          version = "latest";
+          imageFiles = map (sys: inputs.self.packages.${sys}.fdContainer) imageSystems;
+          registries = { };
+          tags = [ "latest" ];
+        };
+      };
+    };
+}
