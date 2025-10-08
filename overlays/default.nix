36337cacdd27bfb755b8cbadd072664616ce8f45
Cameron Smith <cameron.ray.smith@gmail.com>
2025-10-07 22:14:45 -0400

    feat(overlay): expose debug packages via packageDebugging
    Debug packages available as pkgs.packageDebugging.* for build testing
    without overriding nixpkgs versions

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,9 +12,13 @@ let
     };
 
   packageOverrides = fromDirectory ./packages;
+  packageDebugging = fromDirectory ./debug;
 in
 packageOverrides
 // {
+  # Debug packages for nixpkgs maintenance (doesn't override nixpkgs)
+  inherit packageDebugging;
+
   # Additional overrides
   # omnix = inputs.omnix.packages.${self.system}.default;
 }
