f1ab857aac8e18aea70329d385e536ec1fea0c21
Cameron Smith <cameron.ray.smith@gmail.com>
2025-10-09 13:26:31 -0400

    fix(tmux): configure catppuccin for basename paths and minimal status
    Configure catppuccin theme to:
    - Display only basename of current path in window tabs (not full path)
    - Show only time/date on status-right (remove redundant window name)
    
    This properly activates catppuccin's modular configuration system
    which was previously commented out, causing it to use defaults.

--- modules/home/all/terminal/tmux.nix
+++ modules/home/all/terminal/tmux.nix
@@ -147,27 +147,27 @@
       # Right:  (U+E0B4 nf-ple-right_half_circle_thick)
       # Block: █ (U+2588)
       # Zoom:  (U+F531 nf-oct-zoom_in)
+      # Window tab styling
       set -g @catppuccin_window_status_style 'rounded'
       set -g @catppuccin_window_number_position 'right'
-      # set -g @catppuccin_window_status_style 'custom'
-      # set -g @catppuccin_window_left_separator ""
-      # set -g @catppuccin_window_right_separator ""
-      # set -g @catppuccin_window_middle_separator " █"
-      # set -g @catppuccin_window_number_position 'right'
-      # set -g @catppuccin_window_default_fill 'number'
-      # set -g @catppuccin_window_default_text '#W'
-      # set -g @catppuccin_window_current_fill 'number'
-      # set -g @catppuccin_window_current_text '#W#{?window_zoomed_flag,,}'
+      set -g @catppuccin_window_default_fill 'number'
+      set -g @catppuccin_window_current_fill 'number'
 
-      # set -g @catppuccin_status_modules_right 'directory date_time'
-      # set -g @catppuccin_status_modules_left 'session'
-      # set -g @catppuccin_status_left_separator " "
-      # set -g @catppuccin_status_right_separator ""
-      # set -g @catppuccin_status_right_separator_inverse 'no'
-      # set -g @catppuccin_status_fill 'icon'
-      # set -g @catppuccin_status_connect_separator 'no'
-      # set -g @catppuccin_directory_text '#{b:pane_current_path}'
-      # set -g @catppuccin_date_time_text '%H:%M'
+      # Use basename of current path for window names
+      set -g @catppuccin_window_default_text '#{b:pane_current_path}'
+      set -g @catppuccin_window_current_text '#{b:pane_current_path}'
+
+      # Status bar modules - only show time/date on right
+      set -g @catppuccin_status_modules_right 'date_time'
+      set -g @catppuccin_status_modules_left 'session'
+      set -g @catppuccin_status_left_separator ' '
+      set -g @catppuccin_status_right_separator ' '
+      set -g @catppuccin_status_right_separator_inverse 'no'
+      set -g @catppuccin_status_fill 'icon'
+      set -g @catppuccin_status_connect_separator 'no'
+
+      # Date/time format: HH:MM DD-Mon-YY (e.g., 13:19 09-Oct-25)
+      set -g @catppuccin_date_time_text '%H:%M %d-%b-%y'
     '';
   };
 
