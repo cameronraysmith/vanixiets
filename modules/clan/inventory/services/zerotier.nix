6497b66526c75dead7a7b88442bad7988e6769f3
Cameron Smith <cameron.ray.smith@gmail.com>
2025-11-25 20:11:06 -0500

    feat(zerotier): add stibnite to allowedIps for darwin member authorization

--- modules/clan/inventory/services/zerotier.nix
+++ modules/clan/inventory/services/zerotier.nix
@@ -12,6 +12,7 @@
         # External members (darwin machines not managed by clan zerotier service)
         allowedIps = [
           "fddb:4344:343b:14b9:399:930e:e971:d9e0" # blackphos (darwin, member ID: 0ee971d9e0)
+          "fddb:4344:343b:14b9:399:933e:1059:d43a" # stibnite (darwin, member ID: 3e1059d43a)
         ];
       };
     };
