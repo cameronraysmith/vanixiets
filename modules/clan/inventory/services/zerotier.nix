ebcaae00550c4f34daf7a376514dae209ac3a0db
Cameron Smith <cameron.ray.smith@gmail.com>
2025-11-29 00:23:14 -0500

    fix(zerotier): correct rosegold IPv6 address (drop leading zero)

--- modules/clan/inventory/services/zerotier.nix
+++ modules/clan/inventory/services/zerotier.nix
@@ -14,7 +14,7 @@
           "fddb:4344:343b:14b9:399:930e:e971:d9e0" # blackphos (darwin, member ID: 0ee971d9e0)
           "fddb:4344:343b:14b9:399:933e:1059:d43a" # stibnite (darwin, member ID: 3e1059d43a)
           "fddb:4344:343b:14b9:399:93f7:54d5:ad7e" # argentum (darwin, member ID: f754d5ad7e)
-          "fddb:4344:343b:14b9:399:9315:3431:0ee8" # rosegold (darwin, member ID: 1534310ee8)
+          "fddb:4344:343b:14b9:399:9315:3431:ee8" # rosegold (darwin, member ID: 1534310ee8)
         ];
       };
     };
