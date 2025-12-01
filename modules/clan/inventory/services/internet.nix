abc58ad4ab526f8b505a82da149d1bea66a6bde7
Cameron Smith <cameron.ray.smith@gmail.com>
2025-12-01 11:46:09 -0500

    feat(internet): add scheelite public IP for clan deployment
    GCP ephemeral IP: 35.209.246.27
    Note: Update after each enable/deploy cycle as GCP IPs are not static.

--- modules/clan/inventory/services/internet.nix
+++ modules/clan/inventory/services/internet.nix
@@ -2,7 +2,8 @@
   clan.inventory.instances.internet = {
     roles.default.machines.cinnabar.settings.host = "49.13.68.78";
     roles.default.machines.electrum.settings.host = "162.55.175.87";
-    # GCP IP is ephemeral - update after each enable/deploy cycle
+    # GCP IPs are ephemeral - update after each enable/deploy cycle
     roles.default.machines.galena.settings.host = "35.206.81.165";
+    roles.default.machines.scheelite.settings.host = "35.209.246.27";
   };
 }
