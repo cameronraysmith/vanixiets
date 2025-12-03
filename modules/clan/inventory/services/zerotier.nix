1d7124f0339dda431fba534fb00d5bccd93f7b67
Cameron Smith <cameron.ray.smith@gmail.com>
2025-12-02 20:33:13 -0500

    feat(zerotier): add android device to allowedIps
    Member ID: 9fc45d577c

--- modules/clan/inventory/services/zerotier.nix
+++ modules/clan/inventory/services/zerotier.nix
@@ -15,6 +15,7 @@
           "fddb:4344:343b:14b9:399:933e:1059:d43a" # stibnite (darwin, member ID: 3e1059d43a)
           "fddb:4344:343b:14b9:399:93f7:54d5:ad7e" # argentum (darwin, member ID: f754d5ad7e)
           "fddb:4344:343b:14b9:399:9315:3431:ee8" # rosegold (darwin, member ID: 1534310ee8)
+          "fddb:4344:343b:14b9:399:939f:c45d:577c" # android (member ID: 9fc45d577c)
         ];
       };
     };
