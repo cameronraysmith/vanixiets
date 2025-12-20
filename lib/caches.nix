7c6e593916fe3807fe15b271d451e6006d511a01
Cameron Smith <cameron.ray.smith@gmail.com>
2025-12-20 14:13:14 -0500

    chore: reorder individual user caches

--- lib/caches.nix
+++ lib/caches.nix
@@ -5,12 +5,12 @@
     "https://nix-community.cachix.org"
     "https://numtide.cachix.org"
     "https://cache.clan.lol"
-    "https://cameronraysmith.cachix.org"
     "https://pyproject-nix.cachix.org"
     "https://catppuccin.cachix.org"
-    "https://cache.thalheim.io"
     "https://cuda-maintainers.cachix.org"
     "https://nvim-treesitter-main.cachix.org"
+    "https://cameronraysmith.cachix.org"
+    "https://cache.thalheim.io"
   ];
 
   publicKeys = [
@@ -18,11 +18,11 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     "cache.clan.lol-1:3KztgSAB5R1M+Dz7vzkBGzXdodizbgLXGXKXlcQLA28="
-    "cameronraysmith.cachix.org-1:aC8ZcRCVcQql77Qn//Q1jrKkiDGir+pIUjhUunN6aio="
     "pyproject-nix.cachix.org-1:UNzugsOlQIu2iOz0VyZNBQm2JSrL/kwxeCcFGw+jMe0="
     "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-    "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
     "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     "nvim-treesitter-main.cachix.org-1:cbwE6blfW5+BkXXyeAXoVSu1gliqPLHo2m98E4hWfZQ="
+    "cameronraysmith.cachix.org-1:aC8ZcRCVcQql77Qn//Q1jrKkiDGir+pIUjhUunN6aio="
+    "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
   ];
 }
