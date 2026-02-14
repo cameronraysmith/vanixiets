69aa86963aabedd33274ff4ffc98aea3d4a8ec82
Cameron Smith <cameron.ray.smith@gmail.com>
2026-02-14 13:04:24 -0500

    feat(cinnabar): add ntfy.zt Caddy virtual host
    Add ntfy.zt virtual host with dual-stack listenAddresses and
    reverse_proxy to [::1]:2586, following the existing .zt service
    pattern. Caddy handles WebSocket upgrade transparently.

--- modules/machines/nixos/cinnabar/caddy.nix
+++ modules/machines/nixos/cinnabar/caddy.nix
@@ -24,6 +24,16 @@
             reverse_proxy [::1]:8008
           '';
         };
+        "ntfy.zt" = {
+          listenAddresses = [
+            "fddb:4344:343b:14b9:399:93db:4344:343b"
+            "10.147.17.1"
+          ];
+          extraConfig = ''
+            tls internal
+            reverse_proxy [::1]:2586
+          '';
+        };
         "openclaw.zt" = {
           listenAddresses = [
             "fddb:4344:343b:14b9:399:93db:4344:343b"
