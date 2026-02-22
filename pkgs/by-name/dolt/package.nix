4c22e6c3d1e22bc7a26d68aebc9fbbf2343ebc7b
Cameron Smith <cameron.ray.smith@gmail.com>
2026-02-22 13:11:41 -0500

    feat(pkgs): add dolt 1.82.4 with go 1.26 and icu support

---
+++ pkgs/by-name/dolt/package.nix
@@ -0,0 +1,34 @@
+{
+  fetchFromGitHub,
+  icu,
+  lib,
+  buildGoModule,
+  go_1_26,
+}:
+
+(buildGoModule.override { go = go_1_26; }) (finalAttrs: {
+  pname = "dolt";
+  version = "1.82.4";
+
+  src = fetchFromGitHub {
+    owner = "dolthub";
+    repo = "dolt";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-mavL3y+Kv25hzFlDFXk7W/jeKVKlCBjlc67GkL3Jcwk=";
+  };
+
+  modRoot = "./go";
+  subPackages = [ "cmd/dolt" ];
+  vendorHash = "sha256-K1KzsqptZxO5OraWKIXeqKuVSzb6E/Mjy3c5PQ7Rs9k=";
+  proxyVendor = true;
+  doCheck = false;
+
+  buildInputs = [ icu ];
+
+  meta = {
+    description = "Relational database with version control and CLI a-la Git";
+    mainProgram = "dolt";
+    homepage = "https://github.com/dolthub/dolt";
+    license = lib.licenses.asl20;
+  };
+})
