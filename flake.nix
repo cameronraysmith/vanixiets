4a853094590372a723615321b8e8cb5149da49dd
Cameron Smith <cameron.ray.smith@gmail.com>
2026-02-27 12:26:10 -0500

    fix(nixpkgs): point playwright-web-flake to fork with nodejs-slim.src fallback

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
     agenix.inputs.nixpkgs.follows = "nixpkgs";
     agenix.inputs.home-manager.follows = "home-manager";
 
-    playwright-web-flake.url = "github:pietdevries94/playwright-web-flake/1.58.2";
+    playwright-web-flake.url = "github:cameronraysmith/playwright-web-flake/fix-nodejs-src-symlinkjoin";
     playwright-web-flake.inputs.nixpkgs.follows = "nixpkgs";
 
     easykubenix.url = "github:cameronraysmith/easykubenix/dev";
