9e97bfa2ba53c3fb3c2b35be3c903edbae8d7d90
Cameron Smith <cameron.ray.smith@gmail.com>
2026-02-27 23:03:03 -0500

    refactor(clan): strip cameron inventory to infrastructure-only home-manager settings

--- modules/clan/inventory/services/users/cameron.nix
+++ modules/clan/inventory/services/users/cameron.nix
@@ -47,10 +47,9 @@
           # Enable zsh system-wide (works on both platforms)
           programs.zsh.enable = true;
 
-          # Home-Manager: Import crs58 identity module with cameron username
-          # Note: home.homeDirectory is automatically set by crs58 module's conditional:
-          #   - /home/cameron on Linux (cinnabar, electrum)
-          #   - /Users/cameron on Darwin (argentum, rosegold)
+          # Home-Manager infrastructure settings only
+          # Module imports are defined at machine level (argentum, rosegold, etc.)
+          # to avoid duplicate catppuccin imports when clan deploys
           home-manager = {
             useGlobalPkgs = true;
             useUserPackages = true;
@@ -63,29 +62,6 @@
               flake = inputs.self // {
                 inherit inputs;
               };
-            };
-
-            users.cameron = {
-              imports = [
-                inputs.self.modules.homeManager."users/crs58"
-                inputs.self.modules.homeManager.base-sops
-                # Import aggregate modules for crs58/cameron
-                # All aggregates (matches blackphos configuration)
-                inputs.self.modules.homeManager.ai
-                inputs.self.modules.homeManager.core
-                inputs.self.modules.homeManager.development
-                inputs.self.modules.homeManager.packages
-                inputs.self.modules.homeManager.shell
-                inputs.self.modules.homeManager.terminal
-                inputs.self.modules.homeManager.tools
-                # LazyVim home-manager module
-                inputs.lazyvim-nix.homeManagerModules.default
-                # nix-index-database for comma command-not-found (terminal aggregate)
-                inputs.nix-index-database.homeModules.nix-index
-                # agents-md option module (requires flake arg from extraSpecialArgs)
-                ../../../../home/modules/_agents-md.nix
-              ];
-              home.username = "cameron";
             };
           };
         }
