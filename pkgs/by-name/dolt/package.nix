76a1781986d7ccc18be5995142f32581a239ccce
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-04 19:54:32 -0500

    dolt: 1.83.1 -> 1.83.2
    Diff: https://github.com/dolthub/dolt/compare/v1.83.1...v1.83.2

--- pkgs/by-name/dolt/package.nix
+++ pkgs/by-name/dolt/package.nix
@@ -9,18 +9,18 @@
 
 (buildGoModule.override { go = go_1_26; }) (finalAttrs: {
   pname = "dolt";
-  version = "1.83.1";
+  version = "1.83.2";
 
   src = fetchFromGitHub {
     owner = "dolthub";
     repo = "dolt";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-r05MM03/VCLYL4ZKqsKQ9bcUukeLHHJUrdYCf5qBpSs=";
+    hash = "sha256-WKsvKZVn4o870w5sv0owmtm/Od2nhzvZOW/aV1jLysM=";
   };
 
   modRoot = "./go";
   subPackages = [ "cmd/dolt" ];
-  vendorHash = "sha256-RocVRGUELo7PlyCD0dhNOu3l+OVyf/zhdy1GeO4dW88=";
+  vendorHash = "sha256-v3WAiQjYxkzfgoC29M+4U4eG/HNqjdhPkqRGB3ESEgM=";
   proxyVendor = true;
   doCheck = false;
 
