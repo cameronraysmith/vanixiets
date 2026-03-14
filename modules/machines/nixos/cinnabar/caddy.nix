d156a6de2626cfb7a7f60318acba97b43ff4153f
Cameron Smith <cameron.ray.smith@gmail.com>
2026-03-14 16:22:35 -0400

    feat(caddy): inject X-Forwarded-User header for openclaw reverse proxy
    Derive the header value from the openclaw-gateway systemd service user
    to stay in sync with the inventory serviceUser setting.

--- modules/machines/nixos/cinnabar/caddy.nix
+++ modules/machines/nixos/cinnabar/caddy.nix
@@ -1,63 +1,71 @@
 { ... }:
 {
-  flake.modules.nixos."machines/nixos/cinnabar" = {
-    services.caddy = {
-      enable = true;
-      virtualHosts = {
-        "kanban.zt" = {
-          listenAddresses = [
-            "fddb:4344:343b:14b9:399:93db:4344:343b"
-            "10.147.17.1"
-          ];
-          extraConfig = ''
-            tls internal
-            reverse_proxy localhost:3008
-          '';
-        };
-        "beads.zt" = {
-          listenAddresses = [
-            "fddb:4344:343b:14b9:399:93db:4344:343b"
-            "10.147.17.1"
-          ];
-          extraConfig = ''
-            tls internal
-            reverse_proxy localhost:3009
-          '';
-        };
-        "matrix.zt" = {
-          listenAddresses = [
-            "fddb:4344:343b:14b9:399:93db:4344:343b"
-            "10.147.17.1"
-          ];
-          extraConfig = ''
-            tls internal
-            reverse_proxy [::1]:8008
-          '';
-        };
-        "ntfy.zt" = {
-          listenAddresses = [
-            "fddb:4344:343b:14b9:399:93db:4344:343b"
-            "10.147.17.1"
-          ];
-          extraConfig = ''
-            tls internal
-            reverse_proxy [::1]:2586
-          '';
-        };
-        "openclaw.zt" = {
-          listenAddresses = [
-            "fddb:4344:343b:14b9:399:93db:4344:343b"
-            "10.147.17.1"
-          ];
-          extraConfig = ''
-            tls internal
-            reverse_proxy [::1]:18789
-          '';
+  flake.modules.nixos."machines/nixos/cinnabar" =
+    { config, ... }:
+    {
+      services.caddy = {
+        enable = true;
+        virtualHosts = {
+          "kanban.zt" = {
+            listenAddresses = [
+              "fddb:4344:343b:14b9:399:93db:4344:343b"
+              "10.147.17.1"
+            ];
+            extraConfig = ''
+              tls internal
+              reverse_proxy localhost:3008
+            '';
+          };
+          "beads.zt" = {
+            listenAddresses = [
+              "fddb:4344:343b:14b9:399:93db:4344:343b"
+              "10.147.17.1"
+            ];
+            extraConfig = ''
+              tls internal
+              reverse_proxy localhost:3009
+            '';
+          };
+          "matrix.zt" = {
+            listenAddresses = [
+              "fddb:4344:343b:14b9:399:93db:4344:343b"
+              "10.147.17.1"
+            ];
+            extraConfig = ''
+              tls internal
+              reverse_proxy [::1]:8008
+            '';
+          };
+          "ntfy.zt" = {
+            listenAddresses = [
+              "fddb:4344:343b:14b9:399:93db:4344:343b"
+              "10.147.17.1"
+            ];
+            extraConfig = ''
+              tls internal
+              reverse_proxy [::1]:2586
+            '';
+          };
+          "openclaw.zt" = {
+            listenAddresses = [
+              "fddb:4344:343b:14b9:399:93db:4344:343b"
+              "10.147.17.1"
+            ];
+            extraConfig =
+              let
+                openclawUser = config.systemd.services.openclaw-gateway.serviceConfig.User;
+              in
+              ''
+                tls internal
+                reverse_proxy [::1]:18789 {
+                  header_up X-Forwarded-User ${openclawUser}
+                }
+              '';
+          };
         };
       };
-    };
 
-    # only allow https on zerotier interfaces
-    networking.firewall.interfaces."zt+".allowedTCPPorts = [ 443 ];
-  };
+      # only allow https on zerotier interfaces
+      networking.firewall.interfaces."zt+".allowedTCPPorts = [ 443 ];
+    };
 }
