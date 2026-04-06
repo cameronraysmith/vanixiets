156282fccd1196ebc7b6f7467d044d35fcae11b2
Cameron Smith <cameron.ray.smith@gmail.com>
2026-04-06 16:27:29 -0400

    feat(awscli): add B2 and R2 endpoint URL profiles

--- modules/home/tools/awscli.nix
+++ modules/home/tools/awscli.nix
@@ -10,6 +10,13 @@
             region = "us-east-1";
             output = "json";
           };
+          "profile b2" = {
+            endpoint_url = "https://s3.us-east-005.backblazeb2.com";
+          };
+          "profile r2" = {
+            endpoint_url = "https://1ece4a9a8f092f8cbdd679d22b9ecb1f.r2.cloudflarestorage.com";
+            region = "auto";
+          };
         };
       };
     };
