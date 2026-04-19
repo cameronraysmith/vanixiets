4b804eb7609e8dfb204bf92095bf3c2a38887d09
Cameron Smith <cameron.ray.smith@gmail.com>
2026-04-18 21:00:29 -0400

    refactor(dev-shell): source nix-fast-build from nixpkgs

--- modules/dev-shell.nix
+++ modules/dev-shell.nix
@@ -39,7 +39,7 @@
           pkgs.just
           pkgs.nh
           pkgs.nix-output-monitor
-          inputs'.nix-fast-build.packages.default
+          pkgs.nix-fast-build
           pkgs.nix-update
           pkgs.nix-prefetch-github
           # beads-viewer  # disabled: incompatible with dolt backend
