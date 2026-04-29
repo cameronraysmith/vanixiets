417198352bd4ed3e8b34ed1b6967f014a11d520e
Cameron Smith <cameron.ray.smith@gmail.com>
2026-04-29 00:34:56 -0400

    refactor(commands/nix): migrate get-nix-hash to per-file dendritic module

--- modules/home/tools/commands/_nix-tools.nix
+++ modules/home/tools/commands/_nix-tools.nix
@@ -66,42 +66,6 @@
     '';
   };
 
-  # compute sha256 nix hash from URL
-  get-nix-hash = {
-    runtimeInputs = with pkgs; [ nix ];
-    text = ''
-      case "''${1:-}" in
-        -h|--help)
-          cat <<'HELP'
-      Compute SHA256 Nix hash of a file from URL
-
-      Usage: get-nix-hash URL
-
-      Downloads a file from the given URL and computes its SHA256 hash
-      in SRI format for use in Nix expressions.
-
-      Arguments:
-        URL    The URL of the file to hash
-
-      Example:
-        get-nix-hash https://example.com/file.tar.gz
-      HELP
-          exit 0
-          ;;
-        "")
-          echo "Error: URL required" >&2
-          echo "Usage: get-nix-hash URL" >&2
-          echo "Try 'get-nix-hash --help' for more information." >&2
-          exit 1
-          ;;
-      esac
-
-      url="$1"
-      nix_hash=$(nix-prefetch-url "$url")
-      nix hash convert --to sri --hash-algo sha256 "$nix_hash"
-    '';
-  };
-
   # nix garbage collection for both system and user
   ngc = {
     runtimeInputs = with pkgs; [ nix ];
