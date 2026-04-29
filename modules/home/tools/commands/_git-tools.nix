313c221793a5a59c00a0de48cc9a99fff5f48e8a
Cameron Smith <cameron.ray.smith@gmail.com>
2026-04-29 00:22:31 -0400

    refactor(commands/git): migrate gitjsonl to per-file dendritic module

--- modules/home/tools/commands/_git-tools.nix
+++ modules/home/tools/commands/_git-tools.nix
@@ -48,39 +48,6 @@
     '';
   };
 
-  # git log lines as JSON
-  gitjsonl = {
-    runtimeInputs = with pkgs; [
-      git
-      jc
-      nushell
-    ];
-    text = ''
-      case "''${1:-}" in
-        -h|--help)
-          cat <<'HELP'
-      Display git log lines as JSON
-
-      Usage: gitjsonl [LINES]
-
-      Shows specified number of git log entries as JSON in transposed format.
-
-      Arguments:
-        LINES    Number of log entries to show (default: 1)
-
-      Examples:
-        gitjsonl      # Show latest commit as JSON
-        gitjsonl 5    # Show latest 5 commits as JSON
-      HELP
-          exit 0
-          ;;
-      esac
-
-      lines="''${1:-1}"
-      exec nu -c "git log | jc --git-log | from json | take $lines | transpose"
-    '';
-  };
-
   # save staged changes to stash while keeping them staged
   stash-staged = {
     runtimeInputs = with pkgs; [ git ];
